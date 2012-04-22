e5c145ddfce2af372957a5ae6fb8b059f6c1d202
Armen <armen138@gmail.com>
2012-04-22 13:03:22 -0400

    a2d engine files

---
+++ audio.js
@@ -0,0 +1,27 @@
+a2d.Audio = function() {
+	this.channels = [];
+	this.channels.length = 8;
+	this.play = function() {
+		if(!a2d.mute) {
+			var played = false;
+			for(var i = 0; i < this.channels.length; i++){
+				if(this.channels[i].currentTime == 0 || this.channels[i].ended) {
+					this.channels[i].play();
+					played = true;
+					break;
+				}
+			}
+			if(!played) {
+				this.channels[0].pause();
+				this.channels[0].currentTime = 0;
+				this.channels[0].play();
+			}
+		}
+	};
+	this.__defineSetter__("src", function(path) {
+		for(var i = 0; i < this.channels.length; i++) {
+			this.channels[i] = new Audio();
+			this.channels[i].src = path;
+		}
+	});
+};
---
+++ scenenode.js
@@ -0,0 +1,93 @@
+/*global a2d, window */
+/**
+ * Base node. Anything that is displayed inherits from this.
+ * @augments a2d.Events
+ * @augments a2d.Collection
+ * @class
+ */
+//warning: SceneNode pretends to be an array, do not use numerical indices unless you know what you are doing...
+a2d.SceneNode = function () {
+    'use strict';
+    a2d.Collection.apply(this);
+    a2d.Events.apply(this);    
+    var self = this;
+    this.scrollLock = false;      
+    this.opacity = 1.0;
+    /**
+     * bounding box of this node, if applicable. Nodes that display things and want to handle mouse events should take care of setting this.
+     * @type a2d.Rectangle
+     */
+    this.boundingBox = new a2d.Rectangle(new a2d.Position(0, 0), new a2d.Position(0, 0));    
+    /**
+     * True if mouse is currently over this node(and mouse tracking is enabled for this node)
+     * @type boolean
+     */
+    this.hover = false;
+    /**
+     * Scale.
+     * @default (1, 1)
+     * @type a2d.Vector
+     */
+    this.scale = new a2d.Vector(1, 1);
+    /**
+     * Node will not be displayed when this is set to false.
+     * @default true
+     * @type boolean
+     */    
+    this.visible = true;
+    /**
+     * The name of this node. 
+     * @default "SceneNode"
+     * @type string
+     */        
+    this.name = "SceneNode";
+    /**
+     * Find node at given position
+     * @param {a2d.Position} position to look for
+     * @returns node if found, null if not
+     */
+    this.findNodeAt = function(pos) {
+        var searchPos = new a2d.Position(pos.X, pos.Y);
+        if(this.scrollLock) {
+            searchPos.add(a2d.offset);
+        }
+        //look up in reverse drawing order so only the top node is returned
+        for(var i = self.length - 1; i >= 0; i--) {
+            var found = self[i].findNodeAt(searchPos);
+            if(found){
+                return found;
+            }
+        }
+        if(searchPos.isInside(self.boundingBox) && (self.hasEvent("click") || self.hasEvent("mousedown"))) {
+            return self;
+        }
+        return null; 
+    }    
+    /**
+     * Draws this node and its children
+     */     
+    this.draw = function () {
+        var i,
+            mouse = a2d.mousePosition ? new a2d.Position(a2d.mousePosition.X, a2d.mousePosition.Y) : new a2d.Position(0, 0);
+        if(this.scrollLock) {
+            mouse.add(a2d.offset);
+        }        
+        if(mouse && mouse.isInside(this.boundingBox)) {            
+            if(!this.hover) {
+                this.fireEvent("mouseover")
+                this.hover = true
+            }
+        } else {
+            if(this.hover) {
+                this.fireEvent("mouseout");
+                this.hover = false;
+            }
+        }
+        if(this.visible) {
+            for (i = 0; i < this.length; i++) {
+                this[i].draw();
+            }            
+        }
+        //this.fireEvent("draw");
+    };
+};
